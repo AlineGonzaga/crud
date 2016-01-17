6956c1665eaa06b8a429503fe02a3d6b8580cede
Aline Gonzaga <aline22200@gmail.com>
2016-01-17 12:54:54 -0300

    Inserindo outro aluno com sucesso.

--- src/crud/Start.java
+++ src/crud/Start.java
@@ -6,7 +6,8 @@ public static void main(String[] args) {
 		
 	    
 		AlunoPrivado aluno = new AlunoPrivado();
-		aluno.setIdade(20);
+		AlunoPrivado aluno2 = new AlunoPrivado();
+ 		aluno.setIdade(20);
 		aluno.setMatricula("78654378");
 		aluno.setNome("Aline");
 		aluno.setPagou(true);
@@ -24,6 +25,16 @@ public static void main(String[] args) {
 		System.out.println(aluno.getMatricula());
 		System.out.println(aluno.getNome());
 		System.out.println(aluno.isPagou());
+		
+		
+		aluno2.setNome(" Celine ");
+		aluno2.setIdade(17);
+		aluno2.setMatricula("8574975");
+		aluno2.setPagou(true);
+		aluno2.inserirCurso();
+		aluno2.listarCurso();
+		aluno2.editarCurso(3);
+		aluno2.deletarCurso(0);
 		
 		
 //		aluno.cursos[0] = "Matemática";
